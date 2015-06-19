5963357e55b7886c6e93969ef62a8ed1600227e1
hermes829 <hermes829@gmail.com>
2015-06-19 16:47:05 -0400

    defl from bea, conv to real, macro data done

--- R/Data/getData/getCrisp.R
+++ R/Data/getData/getCrisp.R
@@ -16,7 +16,6 @@ crisp = crisp[,c('country','ccode','date','year',crispVars)]
 crisp$country = char(crisp$country)
 crisp$ccode = num(crisp$ccode)
 crisp$polity = crisp$DEMOC - crisp$AUTOC + 11
-crisp = crisp[,setdiff(names(crisp), crispVars)]
 
 # Add in US data...10s all the way through
 ## Polity
@@ -24,16 +23,30 @@ slice = crisp[crisp$country=='FRANCE',]
 slice$country = 'United States';slice$ccode = 2;slice$polity = 10
 ## IMF WEI Dataset
 imf = read.csv(paste0(inPath, 'weoreptc.csv'))
-relVars = c('Population', 'Gross domestic product, constant prices', 'Gross domestic product per capita, constant prices')
-relUnits = c('National currency', 'Persons')
-imfUS = imf %>% .[.$Country=='United States',] %>% .[.$Subject.Descriptor %in% relVars,] %>% .[.$Units %in% relUnits,]
+relVars = c('Population', 'Gross domestic product, current prices', 'Gross domestic product per capita, current prices')
+relUnits = c('U.S. dollars', 'Persons')
+imfUS = imf %>% .[.$Country=='United States',] %>% .[.$Subject.Descriptor %in% relVars,] %>% .[.$Units %in% relUnits,] %>% print()
 names(imfUS)[7:(ncol(imfUS) - 1)] = 1999:2014
 imfUS = imfUS[, c( 'Subject.Descriptor',char(1999:2014) ) ] %>% melt(.,id='Subject.Descriptor') %>% dcast(., variable ~ Subject.Descriptor)
-names(imfUS) = c('year', 'NY.GDP.PCAP.KD', 'NY.GDP.MKTP.KD', 'SP.POP.TOTL')
+names(imfUS) = c('year', 'gdpCapNom', 'gdpNom', 'SP.POP.TOTL')
 for(ii in 1:ncol(imfUS)){ imfUS[,ii] = num(imfUS[,ii]) }
 
-# 999993458   996536.8
+# Bring in deflator from WB to get GDP measures in constant 2005 dollars
+# Defaltor data taken from http://www.bea.gov//national/nipaweb/DownSS2.asp, section 1, table 1.1.9, base year 2009
+usDefl = read.csv(paste0(inPath, 'us_gdp_defl_bea.csv')) 
+imfUS$defl = usDefl$defl[match(imfUS$year, usDefl$year)]
+imfUS$defl05 = imfUS$defl[imfUS$year==2005]
+# Convert GDP figure to real US dollars
+imfUS$NY.GDP.MKTP.KD = imfUS$gdpNom * (imfUS$defl05/imfUS$defl)
+# Calculate real GDP per capita from GDP (billions) and pop data (millions)
+imfUS$NY.GDP.PCAP.KD = (imfUS$NY.GDP.MKTP.KD * 1000000000) / (imfUS$SP.POP.TOTL * 1000000)
 
+# Add in US data to slice
+slice$NY.GDP.MKTP.KD = imfUS$NY.GDP.MKTP.KD[match(slice$year, imfUS$year)]
+slice$NY.GDP.PCAP.KD = imfUS$NY.GDP.PCAP.KD[match(slice$year, imfUS$year)]
+slice$SP.POP.TOTL = imfUS$SP.POP.TOTL[match(slice$year, imfUS$year)]
+
+# bind back into CRISP
 crisp = rbind(crisp, slice)
 
 # Limit to countries in sample
